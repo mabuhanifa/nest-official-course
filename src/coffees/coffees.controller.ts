94663a4eb5659b61618eebfa984386ff297a2fc0
shourovahmd <moh.abuhanifa@gmail.com>
2023-04-15 03:24:31 +0600

    params

--- src/coffees/coffees.controller.ts
+++ src/coffees/coffees.controller.ts
@@ -1,9 +1,13 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 
 @Controller('coffees')
 export class CoffeesController {
-  @Get('new')
+  @Get()
   findAll() {
     return 'hi new';
+  }
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return `new thing from ${id}`;
   }
 }
