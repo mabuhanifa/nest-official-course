76900eca7dc236a1586334ab877a19894101a586
shourovahmd <moh.abuhanifa@gmail.com>
2023-04-15 18:28:23 +0600

    NotFoundException

--- src/coffees/coffees.service.ts
+++ src/coffees/coffees.service.ts
@@ -1,4 +1,4 @@
-import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { Coffee } from './entities/coffee.entity';
 
 @Injectable()
@@ -18,10 +18,7 @@ export class CoffeesService {
   findOne(id: string) {
     const coffee = this.coffees.find((item) => item.id === +id);
     if (!coffee) {
-      throw new HttpException(
-        `Coffee with id: ${id} not found`,
-        HttpStatus.NOT_FOUND,
-      );
+      throw new NotFoundException(`Coffee with id: ${id} not found`);
     }
     return coffee;
   }
