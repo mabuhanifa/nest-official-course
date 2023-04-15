ef9a450ecb8143617961cd91c7b15fb246c6161d
shourovahmd <moh.abuhanifa@gmail.com>
2023-04-15 15:29:15 +0600

    express response

--- src/coffees/coffees.controller.ts
+++ src/coffees/coffees.controller.ts
@@ -6,13 +6,14 @@ import {
   HttpStatus,
   Param,
   Post,
+  Res,
 } from '@nestjs/common';
 
 @Controller('coffees')
 export class CoffeesController {
   @Get()
-  findAll() {
-    return 'hi new';
+  findAll(@Res() response) {
+    response.status(200).send('hello from express response');
   }
   @Get(':id')
   findOne(@Param('id') id: string) {
