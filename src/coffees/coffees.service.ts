a152e03d5dd74a5a05bcc53426ca3544dbea5a62
shourovahmd <moh.abuhanifa@gmail.com>
2023-04-15 17:39:05 +0600

    coffees

--- src/coffees/coffees.service.ts
+++ src/coffees/coffees.service.ts
@@ -1,6 +1,14 @@
 import { Injectable } from '@nestjs/common';
+import { Coffee } from './entities/coffee.entity';
 
 @Injectable()
 export class CoffeesService {
-  private coffees = [];
+  private coffees: Coffee[] = [
+    {
+      id: 1,
+      name: 'capachino',
+      brand: 'blueStar',
+      flavors: ['chocolate', 'vanilla'],
+    },
+  ];
 }
